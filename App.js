d9fdaa2a65e310a2263f93d47a1e9f378bcae29e
koplakzs <atom.go.76@gmail.com>
2023-07-26 17:01:42 +0700

    first

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 export default function App() {
+  const [location, setLocation] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholder="Location"
+        style={styles.input}
+        value={location}
+        onChange={(e) => setLocation(e.target.valueOf)}
+      />
     </View>
   );
 }
@@ -13,8 +18,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: "80%",
+    borderWidth: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    fontSize: 15,
   },
 });
